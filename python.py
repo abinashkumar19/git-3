050e2d070b3a525840a8b3ec64cadd1bbded3f76
abinash <abinashkumar5593@gmail.com>
2025-09-04 14:54:10 +0530

    python.py

---
+++ python.py
@@ -0,0 +1,14 @@
+# ---------------- S3 Bucket ----------------
+resource "aws_s3_bucket" "public_bucket" {
+  bucket = "abinash-public-bucket-${random_integer.rand.result}"
+
+  tags = {
+    Name        = "PublicBucket"
+    Environment = "Dev"
+  }
+}
+
+resource "random_integer" "rand" {
+  min = 10000
+  max = 99999
+}
